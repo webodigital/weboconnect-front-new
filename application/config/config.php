dec3a89185f3ede232bcf7924520f4aa34357e11
sanjeev kumar <thekingman573@gmail.com>
2024-12-18 10:08:33 +0530

    updated

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/weboconnect-front-new/';
+$config['base_url'] = 'https://weboconnect.com/webo_new/';
 
 /*
 |--------------------------------------------------------------------------
