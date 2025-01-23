c8f55b57325ccc4e75f002df27a4d87537f2b377
sanjeev kumar <thekingman573@gmail.com>
2025-01-23 18:04:13 +0530

    updated css

--- application/views/front/common/styles.php
+++ application/views/front/common/styles.php
@@ -12,11 +12,17 @@
 <!-- fontawesome end -->
 
 <!--vendor css -->
-<link rel="stylesheet" href="<?=base_url()?>assets/css/owl.carousel.min.css">
-<link rel="stylesheet" href="<?=base_url()?>assets/css/owl.theme.default.min.css">
-<link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" href="<?=base_url()?>assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="<?=base_url()?>assets/css/owl.theme.default.min.css"> -->
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
+<!-- <link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.min.css"> -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" /> -->
-<link rel="stylesheet" href="<?=base_url()?>assets/css/animate.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 <link rel="stylesheet" href="<?=base_url()?>assets/css/navigation.css">
 <!--vendor css end -->
 
